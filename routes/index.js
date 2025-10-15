b3a1acd794736dcf7e58373f7a9e50afcdc6d437
rafaelozrr <130694052+rafaelozrr@users.noreply.github.com>
2025-10-15 15:49:31 +0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ import { userRouter } from "./userRouter.js";
 
 const router = new Router();
 
-// подключаем роутеры без /api — префикс добавляется в сервере
+
 router.use(userRouter);
 router.use(postRouter);
 
